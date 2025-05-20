3771f9a3d170c11d950b059f6b4921b36f346770
yaejun <chunglaos3@gmail.com>
2025-05-20 21:32:19 +0900

    display할 맵 설정정

--- final_project.py
+++ final_project.py
@@ -1,1 +1,12 @@
-print("hello")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import folium
+from streamlit_folium import st_folium
+from streamlit_gsheets.gsheets_connection import GSheetsConnection
+
+m = folium.Map(location=[37.659845, 126.992394], zoom_start=13)
+
+folium.Marker([37.659845, 126.992394], popup="Seoul City Hall").add_to(m)
+
+st_folium(m, width=700, height=500)
